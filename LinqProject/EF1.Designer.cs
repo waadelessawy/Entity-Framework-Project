2cb833f66c7d47a0609e0451b4bee7638f6814f9
waadelessawy <82984314+waadelessawy@users.noreply.github.com>
2022-04-28 20:50:23 +0200

    edit

--- LinqProject/EF1.Designer.cs
+++ LinqProject/EF1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Waad\source\repos\Linq&EFProjectTest\LinqProject\EF1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\ITI\LinQ\EFProject\EFProject\LinqProject\EF1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
